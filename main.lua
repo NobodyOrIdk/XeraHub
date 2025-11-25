a01d4af4a2a51054cbe9665aafa5597934ed8411
NobodyOrIdk <oleksandrpalamarenko2@gmail.com>
2025-11-25 12:54:27 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local HttpService = game:GetService("HttpService")
 
-local WEBHOOK = "https://discord.com/api/webhooks/1442223867165806725/XMCJcGw8YuguZwbmGTJWy6xoZuY-swSg3pcDu-oc5e6DovI3OKHdKZyUl1Lf_NNy70YY" -- if you so retarded then you could try to spam but it wouldm't do anything.
+local WEBHOOK = "https://discord.com/api/webhooks/1442830678835335289/fYBEPiAJ-Llwmui9NRu0EQuc4pRFxhBqOV2OcwLpi1GjxwGy7tFtV3AXN0Hus-HhRV9g" -- if you so retarded then you could try to spam but it wouldm't do anything.
 
 local function sendWebhook()
     local plr = Players.LocalPlayer
