01862a1c37e75cc3938abbd31ad2d539cf08e6f1
NobodyOrIdk <oleksandrpalamarenko2@gmail.com>
2026-05-04 21:14:34 +0300

    Update warning message to reference outdated.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-warn("Xera Hub is outdated and will be no longer updated! If you wish to run Xera Hub please copy code from main.lua of XeraHub repository and run it manually!")
+warn("Xera Hub is outdated and will be no longer updated! If you wish to run Xera Hub please copy code from outdated.lua of XeraHub repository and run it manually!")
 
 -- Load Plutonium Hub instead
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NobodyOrIdk/PlutoniumHub/refs/heads/main/loader.lua"))()
